a3f2abc3661e822b54dfa15d294b942db2a27d5c
Dongdong Zhang <allenzdd@gmail.com>
2018-05-28 03:28:14 +1000

    v1_

--- Assignment1/Q1_code/reducer.py
+++ Assignment1/Q1_code/reducer.py
@@ -6,6 +6,7 @@ edit by Dongdong Zhang 26 April 2018
 
 import sys
 
+
 def read_map_output(file):
     """ Return an iterator for key, value pair extracted from file (sys.stdin).
     Input format:  category \t video_id, country
@@ -16,6 +17,7 @@ def read_map_output(file):
             continue
         yield line.strip().split("\t")
 
+
 def reducer(Coun1, Coun2):
 
     coun_cat_videoid = {}
@@ -23,7 +25,7 @@ def reducer(Coun1, Coun2):
     for category, video_id_country in read_map_output(sys.stdin):
         video_id, country = video_id_country.strip().split(',')
 
-        #Create 3d dictionary 3rd feature is a [], can add string value
+        # Create 3d dictionary 3rd feature is a [], can add string value
         if category not in coun_cat_videoid:
             coun_cat_videoid[category] = {}
             coun_cat_videoid[category][Coun1] = []
@@ -32,11 +34,11 @@ def reducer(Coun1, Coun2):
         coun_cat_videoid[category][country].append(video_id)
 
     for cat in coun_cat_videoid:
-        #Clear the set and overlap count
+        # Clear the set and overlap count
         set_coun1 = set()
         set_coun2 = set()
         overlap_c1_c2 = 0
-        #Create two class for compute overlap between them
+        # Create two class for compute overlap between them
         set_coun1 = set(coun_cat_videoid[cat][Coun1])
         set_coun2 = set(coun_cat_videoid[cat][Coun2])
 
@@ -51,8 +53,8 @@ def reducer(Coun1, Coun2):
         else:
             c2_prec = 0
 
-        output = "{}; total: {}; {:.1%} in {}".format \
-                (cat, c1_num, float(c2_prec), Coun2)
+        output = "{}; total: {}; {:.1%} in {}".format(
+            cat, c1_num, float(c2_prec), Coun2)
 
         print(output)
 
